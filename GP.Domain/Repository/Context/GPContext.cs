6fae2fe9b7c623363cbfb3a416e534a6cbd7ab1d
jrafael85@gmail.com <jrafael85@gmail.com>
2013-04-25 21:21:40 -0300

    release 1

--- GP.Domain/Repository/Context/GPContext.cs
+++ GP.Domain/Repository/Context/GPContext.cs
@@ -13,38 +13,39 @@ namespace GP.Domain.Repository.Context
         public GPContext()
             : base("DefaultConnectionString")
         {
-            if (!this.Database.Exists())
-            {
-                this.Database.Create();
-
-                this.Features.Add(new Feature() { Name = "Inicio" });
-                this.Features.Add(new Feature() { Name = "Funcionalidade" });
-                this.Features.Add(new Feature() { Name = "Usuario" });
-                this.Features.Add(new Feature() { Name = "Perfil" });           
-
-
-                this.Roles.Add(new Role() { Name = "Convidado" });
-                this.Permissions.Add(new Permission() { Feature = this.Features.Local[0], Role = this.Roles.Local[0], AccessLevelType = RolePermissionType.Read, HierarchyLevelType = HierarchyLevelAccess.Ownership });
-                this.Permissions.Add(new Permission() { Feature = this.Features.Local[1], Role = this.Roles.Local[0], AccessLevelType = RolePermissionType.Read, HierarchyLevelType = HierarchyLevelAccess.Ownership });
-                this.Permissions.Add(new Permission() { Feature = this.Features.Local[2], Role = this.Roles.Local[0], AccessLevelType = RolePermissionType.Read, HierarchyLevelType = HierarchyLevelAccess.Ownership });
-                this.Permissions.Add(new Permission() { Feature = this.Features.Local[3], Role = this.Roles.Local[0], AccessLevelType = RolePermissionType.Read, HierarchyLevelType = HierarchyLevelAccess.Ownership });
-
-
-                this.Roles.Add(new Role() { Name = "Administrador" });
-                this.Permissions.Add(new Permission() { Feature = this.Features.Local[0], Role = this.Roles.Local[1], AccessLevelType = RolePermissionType.Full, HierarchyLevelType = HierarchyLevelAccess.Hierarchy });
-                this.Permissions.Add(new Permission() { Feature = this.Features.Local[1], Role = this.Roles.Local[1], AccessLevelType = RolePermissionType.Full, HierarchyLevelType = HierarchyLevelAccess.Hierarchy });
-                this.Permissions.Add(new Permission() { Feature = this.Features.Local[2], Role = this.Roles.Local[1], AccessLevelType = RolePermissionType.Full, HierarchyLevelType = HierarchyLevelAccess.Hierarchy });
-                this.Permissions.Add(new Permission() { Feature = this.Features.Local[3], Role = this.Roles.Local[1], AccessLevelType = RolePermissionType.Full, HierarchyLevelType = HierarchyLevelAccess.Hierarchy });
+            //if (!this.Database.Exists())
+            //{
+            //    this.Database.Create();
                 
-                this.Users.Add(new User() { FirstName = "Usuario", UserName = "Admin", Password = "Admin", LastName = "Padrão" });
 
-                this.Users.Local[0].Roles = new List<Role>();
-                this.Users.Local[0].Roles.Add(this.Roles.Local[0]);
-                this.Users.Local[0].Roles.Add(this.Roles.Local[1]);
+            //    this.Features.Add(new Feature() { Name = "Inicio" });
+            //    this.Features.Add(new Feature() { Name = "Funcionalidade" });
+            //    this.Features.Add(new Feature() { Name = "Usuario" });
+            //    this.Features.Add(new Feature() { Name = "Perfil" });           
 
-                this.SaveChanges();
 
-            }
+            //    this.Roles.Add(new Role() { Name = "Convidado" });
+            //    this.Permissions.Add(new Permission() { Feature = this.Features.Local[0], Role = this.Roles.Local[0], AccessLevelType = RolePermissionType.Read, HierarchyLevelType = HierarchyLevelAccess.Ownership });
+            //    this.Permissions.Add(new Permission() { Feature = this.Features.Local[1], Role = this.Roles.Local[0], AccessLevelType = RolePermissionType.Read, HierarchyLevelType = HierarchyLevelAccess.Ownership });
+            //    this.Permissions.Add(new Permission() { Feature = this.Features.Local[2], Role = this.Roles.Local[0], AccessLevelType = RolePermissionType.Read, HierarchyLevelType = HierarchyLevelAccess.Ownership });
+            //    this.Permissions.Add(new Permission() { Feature = this.Features.Local[3], Role = this.Roles.Local[0], AccessLevelType = RolePermissionType.Read, HierarchyLevelType = HierarchyLevelAccess.Ownership });
+
+
+            //    this.Roles.Add(new Role() { Name = "Administrador" });
+            //    this.Permissions.Add(new Permission() { Feature = this.Features.Local[0], Role = this.Roles.Local[1], AccessLevelType = RolePermissionType.Full, HierarchyLevelType = HierarchyLevelAccess.Hierarchy });
+            //    this.Permissions.Add(new Permission() { Feature = this.Features.Local[1], Role = this.Roles.Local[1], AccessLevelType = RolePermissionType.Full, HierarchyLevelType = HierarchyLevelAccess.Hierarchy });
+            //    this.Permissions.Add(new Permission() { Feature = this.Features.Local[2], Role = this.Roles.Local[1], AccessLevelType = RolePermissionType.Full, HierarchyLevelType = HierarchyLevelAccess.Hierarchy });
+            //    this.Permissions.Add(new Permission() { Feature = this.Features.Local[3], Role = this.Roles.Local[1], AccessLevelType = RolePermissionType.Full, HierarchyLevelType = HierarchyLevelAccess.Hierarchy });
+                
+            //    this.Users.Add(new User() { FirstName = "Usuario", UserName = "Admin", Password = "Admin", LastName = "Padrão" });
+
+            //    this.Users.Local[0].Roles = new List<Role>();
+            //    this.Users.Local[0].Roles.Add(this.Roles.Local[0]);
+            //    this.Users.Local[0].Roles.Add(this.Roles.Local[1]);
+
+            //    this.SaveChanges();
+
+            //}
         }
 
 
